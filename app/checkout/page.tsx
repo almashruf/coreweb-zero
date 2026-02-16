48fd9b6760416dc57f5b3de9fa768964b9596fda
almashruf <tonoy5300@gmail.com>
2026-02-16 22:38:16 +0600

    branch-03: FCP optimization

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -6,3 +6,35 @@ export default function Checkout() {
     </main>
   );
 }
+
+// need to keep main content as server component
+
+// export default function Home(){
+//     return <h1>Our Store</h1>;//html direct 
+// }
+
+// "use client "
+
+// export default function Home(){
+//     return <h1>Our Store</h1>;// need js to render
+// }
+
+// import { Inter } from "next/font/google";
+
+// const inter = Inter({ subsets: ["latin"] });
+
+// export default function RootLayout({
+//   children,
+// }: {
+//   children: React.ReactNode;
+// }) {
+//   return (
+//     <html lang="en">
+//       <body className={inter.className}>
+//         {children}
+//       </body>
+//     </html>
+//   );
+// }
+
+// here next/font self-hosts fonts, no external request
